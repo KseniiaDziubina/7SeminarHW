3c9d28ead73786b455d23471f7ef3a320ca583f8
Kseniia <ksenjadzjubina@gmail.com>
2022-08-04 16:20:53 +0300

    Задача 47

---
+++ Task47/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите количество строк в массиве:  ");
+int Line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве:  ");
+int Column = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = new double[Line, Column];
+
+for (int i = 0; i < Line; i++)
+    {
+      for (int j = 0; j < Column; j++)
+      {
+        myArray[i,j] = new Random().NextDouble()*100;
+        Console.Write("{0,6:F2}", myArray[i,j]);
+      }
+      Console.WriteLine();
+    }

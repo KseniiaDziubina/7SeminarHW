f37c7d90019d98154fc05f6daed3e25292f83d2a
Kseniia <ksenjadzjubina@gmail.com>
2022-08-04 16:33:45 +0300

    Задача 52

---
+++ Task52/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите количество строк в массиве:  ");
+int Line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве:  ");
+int Column = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = new int[Line, Column];
+Random range = new Random();
+for (int j = 0; j < Column; j++)
+    {
+        double Sum = 0;
+        for (int i = 0; i < Line; i++)
+        {
+            Array[i,j] = range.Next(0, 100);
+            Sum += Array[i,j];
+        }
+        Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {Math.Round((Sum/Column),3)}");
+    }
